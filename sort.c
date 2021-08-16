06096b3d54bd9e69e3a37fc2536203ecf8b0ba28
eric29200 <eric_begot@yahoo.fr>
2021-08-16 17:08:17 +0200

    implement quick sort

--- sort.c
+++ sort.c
@@ -43,6 +43,9 @@ void sort_bubble(int *array, int nb_elements)
 {
   int change = 1, i;
 
+  if (nb_elements < 2)
+    return;
+
   while (change) {
     change = 0;
 
@@ -62,6 +65,9 @@ void sort_bubble(int *array, int nb_elements)
 void sort_insertion(int *array, int nb_elements)
 {
   int i, j, min;
+
+  if (nb_elements < 2)
+    return;
 
   for (i = 0; i < nb_elements; i++) {
     min = i;
@@ -103,6 +109,9 @@ void sort_heap(int *array, int nb_elements)
 {
   int i;
 
+  if (nb_elements < 2)
+    return;
+
   /* "heapify" the array */
   for (i = nb_elements / 2 - 1; i >= 0; i--)
     heapify(array, nb_elements, i);
@@ -112,6 +121,41 @@ void sort_heap(int *array, int nb_elements)
     swap(&array[0], &array[i]);
     heapify(array, i, 0);
   }
+}
+
+/*
+ * Quick sort.
+ */
+void sort_quick(int *array, int nb_elements)
+{
+  int i, j, pivot;
+
+  if (nb_elements < 2)
+    return;
+
+  /* select pivot = middle element */
+  pivot = array[nb_elements / 2];
+
+  for (i = 0, j = nb_elements - 1; ; i++, j--) {
+    /* find first element less than pivot */
+    while (array[i] < pivot)
+      i++;
+
+    /* find last element greater than pivot */
+    while (array[j] > pivot)
+      j--;
+
+    /* already sorted */
+    if (i >= j)
+      break;
+
+    /* swap elements */
+    swap(&array[i], &array[j]);
+  }
+
+  /* divide */
+  sort_quick(array, i);
+  sort_quick(array + i, nb_elements - i);
 }
 
 /*
@@ -155,6 +199,9 @@ int main()
 
   /* heap sort */
   sort(array, NB_ELEMENTS, sort_heap, "Heap sort");
+
+  /* quick sort */
+  sort(array, NB_ELEMENTS, sort_quick, "Quick sort");
 
   return 0;
 }

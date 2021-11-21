fc7293ceaade396069ecf11b3f1989214d50eb78
eric29200 <eric_begot@yahoo.fr>
2021-11-21 11:53:28 +0100

    sort benchmarks

--- algo.c
+++ algo.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
 #include "sort/sort_bubble.h"
 #include "sort/sort_insertion.h"
@@ -8,7 +10,7 @@
 #include "sort/sort_heap.h"
 #include "utils/mem.h"
 
-#define NB_ELEMENTS   2000000
+#define NB_ELEMENTS   20000000
 
 /*
  * Integer comparison.
@@ -16,6 +18,33 @@
 static inline int int_compare(const void *a1, const void *a2)
 {
   return *((int *) a1) - *((int *) a2);
+}
+
+/*
+ * Sort test.
+ */
+static void sort_test(void *data, const char *method, void (*sort)(void *, size_t, size_t, int (*compare)()))
+{
+  clock_t start, end;
+  double time;
+  void *tmp;
+
+  /* copy data */
+  tmp = xmalloc(NB_ELEMENTS * sizeof(int));
+  memcpy(tmp, data, NB_ELEMENTS * sizeof(int));
+
+  /* sort */
+  start = clock();
+  sort(tmp, NB_ELEMENTS, sizeof(int), int_compare);
+  end = clock();
+  time = (double) (end - start) / CLOCKS_PER_SEC;
+
+  /* free tmp data */
+  free(tmp);
+
+  /* print statistics */
+  printf("******* %s\n", method);
+  printf("Sort time : %f\n", time);
 }
 
 /*
@@ -31,18 +60,14 @@ int main(int argc, char **argv)
   for (i = 0; i < NB_ELEMENTS; i++)
     data[i] = rand() % 10;
 
-  /* print data */
-  //for (i = 0; i < NB_ELEMENTS; i++)
-  //  printf("%d|", data[i]);
-  //printf("\n");
+  /* heap sort */
+  sort_test(data, "Heap sort", sort_heap);
 
-  /* sort array */
-  sort_quick(data, NB_ELEMENTS, sizeof(int), int_compare);
+  /* quick sort */
+  sort_test(data, "Quick sort", sort_quick);
 
-  /* print data */
-  //for (i = 0; i < NB_ELEMENTS; i++)
-  //  printf("%d|", data[i]);
-  //printf("\n");
+  /* merge sort */
+  sort_test(data, "Merge sort", sort_merge);
 
   /* free data */
   free(data);
